ae3a8f64978cc19fa515d71716ef919545a417d9
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-28 20:05:03 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,5 +9,10 @@ def countdown(number)
 end
 
 def countdown_with_sleep(number)
-  sleep 5
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
 end
